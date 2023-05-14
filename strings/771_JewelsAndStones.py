eddc6e3db525d1138e08b1cda852962c1def3f53
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-14 18:45:55 +0530

    Added Optimised code

---
+++ strings/771_JewelsAndStones.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count = 0
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
