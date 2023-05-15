77e55c0ee5c979f9dc243dca73791c971f23b60a
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-15 21:49:40 +0530

    Added Optimised code

--- strings/1768_MergeStringsAlternately.py
+++ strings/1768_MergeStringsAlternately.py
@@ -5,18 +5,13 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        out=""
-        if len(word1)>len(word2):
-            max=len(word1)
-            min=len(word2)
-            word = word1
-        else:
-            max=len(word2)
-            min = len(word1)
-            word = word2
-        for i in range(0,min):
-            out = out + word1[i]
-            out = out + word2[i]
-        for i in range(min,max):
-            out = out + word[i]
-        return out
+        i, j = 0, 0
+        res = []
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+        res.append(word1[i:])
+        res.append(word2[j:])
+        return "".join(res)
