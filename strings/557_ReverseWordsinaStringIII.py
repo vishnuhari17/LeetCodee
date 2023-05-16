7af6d41c2f62c20e974d57ca4f55efe089d30d1b
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-16 19:49:47 +0530

    Added Optimised code

--- strings/557_ReverseWordsinaStringIII.py
+++ strings/557_ReverseWordsinaStringIII.py
@@ -4,15 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        out = ""
         list=[]
-        count = 0
         for i in s.split():
-            count += 1
-            for j in i:
-                list.append(j)
-            out = out + "".join(list[::-1])
-            list= []
-            if count != len(s.split()):
-                out = out + " "
-        return out
+            list.append(i[::-1])
+        return " ".join(list)
