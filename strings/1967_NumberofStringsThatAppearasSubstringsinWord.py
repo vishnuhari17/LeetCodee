2256a57215a57b711e87fbf5a7c0b2a9e3127f45
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-17 20:00:58 +0530

    Added solution

---
+++ strings/1967_NumberofStringsThatAppearasSubstringsinWord.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numOfStrings(self, patterns, word):
+        """
+        :type patterns: List[str]
+        :type word: str
+        :rtype: int
+        """
+        count = 0
+        for i in patterns:
+            if i in word:
+                count += 1
+        return count
