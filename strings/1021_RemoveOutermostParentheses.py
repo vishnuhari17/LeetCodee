7b2b06eab22c81f65c09af969b2dbc7a3347539e
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-17 15:19:19 +0530

    Tried but failed

--- strings/1021_RemoveOutermostParentheses.py
+++ strings/1021_RemoveOutermostParentheses.py
@@ -1,13 +1,19 @@
-o=0
-c=0
-out = ""
-s = "(()())(())(()(()))"
-for i in s:
-    if i == "(":
-        o += 1
-    else:
-         c += 1
-    if o == c:
-        out = out + "()"*(o//2)
-print(out)
-#not reaching the ans still :(
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        memory=[]
+        out = ""
+        o=0
+        for i in s:
+            if i == "(":
+                o += 1
+            else:
+                o -= 1
+            memory.append(i)
+            if o == 0:
+                out = out + "".join(memory[1:len(memory)-1])
+                memory = []
+        return out
