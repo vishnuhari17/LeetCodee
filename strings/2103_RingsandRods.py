48a4838ef8ce6f0bfd57a1e0a6fa3f48080803a8
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-17 17:46:36 +0530

    Added solution

--- strings/2103_RingsandRods.py
+++ strings/2103_RingsandRods.py
@@ -1,4 +1,19 @@
-rings = "B0B6G0R6R0R6G9"
-for i in range(0,len(rings)+1,2):
-
-    #to be revisited 
+class Solution(object):
+    def countPoints(self, rings):
+        """
+        :type rings: str
+        :rtype: int
+        """
+        dict={}
+        count = 0
+        for i in range(0,len(rings)-1,2):
+            j=dict.get(rings[i+1])
+            if j == None:
+                dict[rings[i+1]]=[rings[i]]
+            else:
+                if rings[i] not in j:
+                    j.append(rings[i])
+                    if len(j)==3:
+                        count += 1
+                    dict[rings[i + 1]]=j
+        return count
