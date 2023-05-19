f3f7a8c71a9bd75cf23ba851125c7fc2dd85a871
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-19 17:34:33 +0530

    Added solution

--- strings/1332_RemovePalindromicSubsequences.py
+++ strings/1332_RemovePalindromicSubsequences.py
@@ -1,23 +1,21 @@
-s = "baabb"
-
-count = 0
-def pal(s):
-    list = []
-    for i in s:
-        list.append(i)
-    list.reverse()
-    if s == "".join(list):
-        return True
-for i in range(len(s)):
-    if pal(s) == True:
-        count += 1
-        break
-    elif pal(s[i:])==True:
-        s = s[i:]
-        count +=1
-    elif pal(s.lstrip()) == True:
-        s = s[:i]
-        count += 1
-print(count)
-
-
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        count = 0
+        def pal(s):
+            list = []
+            for i in s:
+                list.append(i)
+            list.reverse()
+            if s == "".join(list):
+                return True
+        for i in range(len(s)):
+            if pal(s) == True:
+                count += 1
+                break
+            elif pal(s[i:])==True:
+                s = s[i:]
+                count +=1
+            elif pal(s.lstrip()) == True:
+                s = s[:i]
+                count += 1
+        return count
