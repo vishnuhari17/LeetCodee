e6ab85ce9474d6d1d2c43b0bad11e6555dd1066c
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-22 22:27:10 +0530

    Added solution

--- strings/2283_CheckifNumberHasEqualDigitCountandDigitValue.py
+++ strings/2283_CheckifNumberHasEqualDigitCountandDigitValue.py
@@ -1,13 +1,9 @@
 class Solution:
     def digitCount(self, num: str) -> bool:
-        dict = {}
-        for i in num:
-            if int(i) not in dict:
-                dict[int(i)] = 0
-            dict[int(i)] += 1
-        for i in range(0,len(num)):
-            if dict.get(int(i)) == None and int(num[i]) == 0:
-                continue
-            elif dict.get(int(i)) != int(num[i]):
+        counts = [0] * 10
+        for digit in num:
+            counts[int(digit)] += 1
+        for i in range(len(num)):
+            if counts[i] != int(num[i]):
                 return False
         return True
