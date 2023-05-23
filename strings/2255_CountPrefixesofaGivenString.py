dd97fcdadf54160d64a59a57fb7796e61d33eb5a
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-23 14:26:48 +0530

    Added solution

--- strings/2255_CountPrefixesofaGivenString.py
+++ strings/2255_CountPrefixesofaGivenString.py
@@ -5,3 +5,13 @@ class Solution:
             if s[:i] in words:
                 count += words.count(s[:i])
         return count
+
+# Another approach
+
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        count = 0
+        for word in words:
+            if word == s[:len(word)]:
+                count += 1
+        return count
