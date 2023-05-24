215327a2cd3040218a7c306d5c643b77da5659e5
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2023-05-24 22:43:09 +0530

    Added solution

---
+++ arrays/1929_ConcatenationofArray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in range(2):
+            for i in nums:
+                ans.append(i)
+        return ans
